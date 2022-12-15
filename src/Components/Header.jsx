65d95a6bcebb6d4cb96488deda4e29d9e42f8ee6
madinaalizada <lizadmdin@gmail.com>
2022-12-15 05:40:01 +0400

    added poppits

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import bellIcon from '../images/header/notification.svg';
-import calendar from '../images/header/calendar.svg';
-import mail from '../images/header/message.svg';
+import calendar from '../images/header/note-text.svg';
+import mail from '../images/header/sms.svg';
 import mountain from '../images/header/mountain.svg'
 import vector from '../images/header/Vector.svg'
 import './Header.css';
@@ -12,9 +12,16 @@ const Header = () => {
         <div className="header-icons">
             <h6>Ana səhifə</h6>
             <div className="icons">
+              {/* <div className=""> */}
                 <img src={bellIcon} alt="icon" className='icon'/>
+              {/* </div> */}
+              {/* <div className=""> */}
                 <img src={calendar} alt="icon" className='icon'/>
-                <img src={mail} alt="icon" className='icon'/>
+              {/* </div> */}
+                <div className='mail-icon'>
+                  <img src={mail} alt="icon" className='icon'/>
+                  <div className="mail-messages">12</div>
+                </div>
             </div>
         </div>
         <div className="profile">
